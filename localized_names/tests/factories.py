8cb9e753024cf6c8d524cb99a697bbb70f5dcf7e
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-05-07 19:34:41 +0200

    Added DummyModel and factory.

--- localized_names/tests/factories.py
+++ localized_names/tests/factories.py
@@ -2,3 +2,22 @@
 Factories for the models of the ``localized_names`` app and its ``test_app``.
 
 """
+import factory
+
+from .test_app.models import DummyModel
+
+
+class DummyModelFactory(factory.Factory):
+    """Factory for the ``DummyModel`` model."""
+    FACTORY_FOR = DummyModel
+
+    title = 'Mr.'
+    romanized_first_name = factory.Sequence(
+        lambda i: 'romanized_first_name{0}'.format(i))
+    romanized_last_name = factory.Sequence(
+        lambda i: 'romanized_last_name{0}'.format(i))
+    non_romanized_first_name = factory.Sequence(
+        lambda i: 'non_romanized_first_name{0}'.format(i))
+    non_romanized_last_name = factory.Sequence(
+        lambda i: 'non_romanized_last_name{0}'.format(i))
+    nickname = factory.Sequence(lambda i: 'nickname{0}'.format(i))
--- localized_names/tests/test_app/models.py
+++ localized_names/tests/test_app/models.py
@@ -1,1 +1,30 @@
 """Models for testing purposes."""
+from django.db import models
+
+
+class DummyModel(models.Model):
+    """A model for testing only."""
+    title = models.CharField(max_length=64)
+    romanized_first_name = models.CharField(max_length=64)
+    romanized_last_name = models.CharField(max_length=64)
+    non_romanized_first_name = models.CharField(max_length=64)
+    non_romanized_last_name = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=64)
+
+    def get_title(self):
+        return self.title
+
+    def get_romanized_first_name(self):
+        return self.romanized_first_name
+
+    def get_romanized_last_name(self):
+        return self.romanized_last_name
+
+    def get_non_romanized_first_name(self):
+        return self.non_romanized_first_name
+
+    def get_non_romanized_last_name(self):
+        return self.non_romanized_last_name
+
+    def get_nickname(self):
+        return self.nickname
