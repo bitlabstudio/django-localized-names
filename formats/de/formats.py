b5f45c57c005070d64986a0e4e5b5a96f9e873f3
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-05-07 19:44:05 +0200

    Added default formats for German, English and Chinese.

---
+++ formats/de/formats.py
@@ -0,0 +1,4 @@
+"""Formats for the German language."""
+
+LONG_NAME_FORMAT = '{t} {f} {l}'
+SHORT_NAME_FORMAT = '{f} {l}'
---
+++ formats/zh/formats.py
@@ -0,0 +1,4 @@
+"""Formats for the Chinese language."""
+
+LONG_NAME_FORMAT = '{a}{x}{t} ({L}, "{n}", {f})'
+SHORT_NAME_FORMAT = '{a}{x}'
